4057839128f038f033ec838d3dffee896ae89d71
André Fonseca <andre.fonseca@rethink.dev>
2023-06-28 14:31:28 -0300

    feat: creating locomotive routes route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { router as locomotivesRoutes } from "./locomotives";
+import { router as routesRoutes } from "./routes";
 
 const router: Router = Router();
 
 router.use("/locomotives", locomotivesRoutes);
+router.use("/routes", routesRoutes);
 
 export { router };
