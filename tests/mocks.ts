c556f3fe8a0d0a043fbde11f6bd555df13699a0a
Luiz Arnoni Fraga <luiz.arnoni@seguranca.mg.gov.br>
2023-06-29 16:20:42 -0300

    tests: communicationReports

--- tests/mocks.ts
+++ tests/mocks.ts
@@ -16,3 +16,47 @@ export const routeData: RoutesType = {
   latitude: "-12.8531",
   longitude: "-38.1615",
 };
+
+export const mockReports = [
+  {
+    id: 1,
+    subject: "Report 1",
+    created_by_id: 1,
+    created_at: "2023-06-26T18:03:32.909Z",
+    driver_id: 1,
+    location: "Location 1",
+    description: "Description 1",
+    locomotive_id: 1,
+    is_stopped: true,
+    reason_stopped: "Reason 1",
+    is_communication_failed: false,
+  },
+];
+
+export const mockNewReport = {
+  subject: "Report 2",
+  created_by_id: 2,
+  driver_id: 2,
+  location: "Location 2",
+  description: "Description 2",
+  locomotive_id: 2,
+  is_stopped: false,
+  reason_stopped: "Reason 2",
+  is_communication_failed: true,
+};
+
+export const mockReportFormated = {
+  id: 3,
+  subject: "lsidasoidhsaiusahiudas",
+  created_by_id: null,
+  created_at: "2023-06-29T17:37:36.775Z",
+  driver_id: 3,
+  location: "lalla",
+  description: "lalala",
+  locomotive_id: 9,
+  is_stopped: false,
+  reason_stopped: "lalala",
+  is_communication_failed: true,
+  date: "29/06/2023",
+  time: "14:37:36",
+};
